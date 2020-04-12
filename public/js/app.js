a8bf11054beb8b062ca7c42bf9d512b63f7309aa
jo3y <joey.iphone@me.com>
2020-04-12 10:45:14 +1000

    removed console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,7 @@
-console.log(`client side js file loaded`)
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
